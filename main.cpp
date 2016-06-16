113b6d48e01f6094e18c467fd60c668721e53026
Atrixium <rob.ss.bass@gmail.com>
2016-06-15 23:06:35 -0700

    Added Hello World code, corrected vicious bug with debug mode not linking correctly by clearing linker settings from the debug section and using only project level linker option -lSDL2

--- main.cpp
+++ main.cpp
@@ -2,52 +2,125 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+// Global Constants
 
-int main(int argc, char** args[] ){
+    const int SCREEN_WIDTH = 640;
+    const int SCREEN_HEIGHT = 480;
 
-//The window we'll be rendering to
-SDL_Window* window = NULL;
+// Forward Declarations
 
-//The surface contained within the window
-SDL_Surface* screenSurface = NULL;
+    // Starts up SDL and creates a window
+    bool init();
 
-//Initialize SDL
-if( SDL_Init( SDL_INIT_VIDEO ) <0 )
+    // Loads media
+    bool loadMedia();
+
+    //Frees media and shuts down SDL
+    void kill();
+
+// Gloabal variables
+
+    // The window we will be rendering to
+    SDL_Window* gWindow = NULL;
+
+    //The surface contained by the window
+    SDL_Surface* gScreenSurface = NULL;
+
+    //The image we will load and show on the screen
+    SDL_Surface* gHelloWorld = NULL;
+
+
+int main(int argc, char* args[] )
+{
+    //Start SDL and create window
+    init();
+    if( !init() )
     {
-        printf( "SDL could not initialize! SDL Error: %s/n", SDL_GetError() );
+        printf( "Failed to initialize!\n" );
     }
-else
+    else
     {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-            {
-                printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-            }
+        //load media
+        loadMedia();
+        if( !loadMedia() )
+        {
+            printf( "Failed to load media!\n" );
+        }
         else
-            {
-                //Get window surface
-                screenSurface = SDL_GetWindowSurface( window );
+        {
+            //Apply the image
+            SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
 
-                //Fill the surface white
-                SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            //Update the surface
+            SDL_UpdateWindowSurface( gWindow );
 
-                //Update the surface
-                SDL_UpdateWindowSurface( window );
-
-                //Wait two seconds
-                SDL_Delay( 2000 );
-            }
+            //wait 2 seconds
+            SDL_Delay( 2000 );
+        }
     }
 
-//Destroy window
-SDL_DestroyWindow( window );
+    //Free resources and close SDL
+    kill();
 
-//Quit SDL subsystems
-SDL_Quit();
+    return 0;
 
-return 0;
+}
 
+bool init()
+{
+    //Initiaslization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not intialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create Window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL)
+        {
+            printf( "Window could not be created!! SDL_Error:%s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+    }
+    return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load splash image
+    gHelloWorld = SDL_LoadBMP( "images/hello_world.bmp" );
+    if( gHelloWorld == NULL )
+    {
+        printf( "Unable to load image: %s!\nSDL_Error: %s\n", "images/hello_world.bmp", SDL_GetError() );
+        success = false;
+    }
+
+    return success;
+}
+
+void kill()
+{
+    //Deallocate surface
+    SDL_FreeSurface( gHelloWorld );
+    gHelloWorld = NULL;
+
+    //destroy window
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+
+    //quit SDL subsystems
+    SDL_Quit();
 }
