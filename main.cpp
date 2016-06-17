1847f564530c5f7032137794e6e05a11bf14bc82
rob <rob.ss.bass@gmail.com>
2016-06-17 13:16:05 -0700

    Working on xing out window

--- main.cpp
+++ main.cpp
@@ -30,6 +30,7 @@
     SDL_Surface* gHelloWorld = NULL;
 
 
+
 int main(int argc, char* args[] )
 {
     //Start SDL and create window
@@ -48,14 +49,32 @@ int main(int argc, char* args[] )
         }
         else
         {
-            //Apply the image
-            SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
 
-            //Update the surface
-            SDL_UpdateWindowSurface( gWindow );
+            //Main loop flag
+            bool quit = false;
 
-            //wait 2 seconds
-            SDL_Delay( 2000 );
+            //Event handler
+            SDL_Event e;
+
+            while( !quit )
+            {
+                //Handle events on queue
+                while( SDL_PollEvent( &e ) !=0 )
+                {
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
+                }
+
+                //Apply the image
+                SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+
+                //Update the surface
+                SDL_UpdateWindowSurface( gWindow );
+
+            }
         }
     }
 
